58337d66dbb6ac1247a96b6e92c47e14583c3b3f
Cornelius Ahner <cornelius@example.com>
2026-08-14 21:16:01 +0200

    feat(icon): update icon URL endpoint to serve SVGs from the API

--- public/js/icons.js
+++ public/js/icons.js
@@ -103,7 +103,7 @@ window.icons = {
   },
 
   url(id) {
-    return `https://cdn.jsdelivr.net/npm/simple-icons@11/icons/${id}.svg`;
+    return `/api/icon/simpleicon/${id}`;
   },
 
   dashboardUrl(name, format = 'png', variant = '') {
